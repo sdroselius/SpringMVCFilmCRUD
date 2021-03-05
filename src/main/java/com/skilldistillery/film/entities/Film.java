83e59c1241900bdf498b18a87409c5205887b9dd
Rob Roselius <roselius@skilldistillery.com>
2021-03-05 16:43:28 -0700

    Add title property to Film

--- src/main/java/com/skilldistillery/film/entities/Film.java
+++ src/main/java/com/skilldistillery/film/entities/Film.java
@@ -2,4 +2,12 @@ package com.skilldistillery.film.entities;
 
 public class Film {
 	private int id;
+	private String title;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
 }
