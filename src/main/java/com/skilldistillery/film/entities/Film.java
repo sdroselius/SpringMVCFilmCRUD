b171173ebc1eb94d6ef009447acf04e7f955acb1
Jeremy Botta <jeremy.r.botta@gmail.com>
2021-03-05 16:43:10 -0700

    getters and setters for id

--- src/main/java/com/skilldistillery/film/entities/Film.java
+++ src/main/java/com/skilldistillery/film/entities/Film.java
@@ -2,4 +2,12 @@ package com.skilldistillery.film.entities;
 
 public class Film {
 	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 }
