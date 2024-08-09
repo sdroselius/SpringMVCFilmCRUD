be76c80073bf81667606563c3f910acedfdb3141
Rob Roselius <roselius@skilldistillery.com>
2024-08-09 17:08:29 -0600

    Stub out DAO/Impl, autowire in controller

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,10 +1,16 @@
 package com.skilldistillery.film.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.skilldistillery.film.dao.FilmDAO;
+
 @Controller
 public class FilmController {
+	
+	@Autowired
+	private FilmDAO filmDao;
 	
 	@RequestMapping( path={"index.do", "/"} )
 	public String index() {
