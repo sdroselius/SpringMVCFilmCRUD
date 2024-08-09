d3cc5c11769870d0f8a36ed1cfefa11d626e0e5f
Rob Roselius <roselius@skilldistillery.com>
2024-08-09 16:17:05 -0600

    Create FilmController

---
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.film.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FilmController {
+	
+	@RequestMapping( path={"index.do", "/"} )
+	public String index() {
+		return "index";
+	}
+
+}
